8be6496df3a3901f65882c3e7a3131dd0c67c28e
Ian Flurkey <ianflurkey@gmail.com>
2022-12-05 12:17:26 -0500

    commit tree 1f317b55f45a75b3aac3eeabdcd930e8e43c8e19

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat (catName) {
+    cats.push(catName);
+}
+
+function destructivelyPrependCat (catName) {
+    cats.unshift(catName);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat (catName) {
+    return [...cats, catName];
+}
+
+function prependCat (catName) {
+    return [catName, ...cats];
+}
+
+function removeFirstCat () {
+    return cats.slice(1);
+}
+
+function removeLastCat(){
+    return cats.slice(0, cats.length-1);
+}
